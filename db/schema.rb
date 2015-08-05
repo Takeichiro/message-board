755a10ae7225b8593bad6d5b849f1532540923be
takeichiro <takeichiroshima@gmail.com>
2015-08-05 08:23:29 +0000

    create message model

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150805073936) do
+ActiveRecord::Schema.define(version: 20150805081616) do
 
   create_table "massages", force: :cascade do |t|
+    t.string   "name"
+    t.string   "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
     t.string   "name"
     t.string   "body"
     t.datetime "created_at", null: false
