de6bf585d3e5b5ace8fc563a7de8910a00ab3ffb
takeichiro <takeichiroshima@gmail.com>
2015-08-09 10:49:56 +0000

    edit

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,21 @@
 class MessagesController < ApplicationController
+    before_action :set_message, only: [:edit, :update]
   def index
     @messages = Message.all
     @message = Message.new
   end
   
+  def edit
+  end
+  
+  def update
+    if @message.update(message_params)
+        redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+        render 'edit'
+    end
+  end
+        
   def create
    @message = Message.new(message_params)
    
@@ -21,4 +33,7 @@ class MessagesController < ApplicationController
     params.require(:message).permit(:name, :body)
   end
   
+  def set_message
+      @message = Message.find(params[:id])
+  end
 end
